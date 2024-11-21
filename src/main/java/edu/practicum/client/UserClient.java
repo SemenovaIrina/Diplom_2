49766226fa21ac184954d4c327f7b144620131c8
Irina Semenova <semenova.iv.ssau@ya.ru>
2024-11-21 09:14:11 +0400

    create class-client (methods for work with handles) for User

--- src/main/java/edu/practicum/client/UserClient.java
+++ src/main/java/edu/practicum/client/UserClient.java
@@ -1,2 +1,39 @@
-package edu.practicum.client;public class UserClient {
+package edu.practicum.client;
+
+import edu.practicum.data.Constants;
+import edu.practicum.models.User;
+import edu.practicum.models.UserForLogin;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class UserClient {
+    private static RequestSpecification request = given(Constants.REQUEST_SPEC);
+
+    @Step("Creating a user with POST request to /api/auth/register")
+    public static Response create(User user) {
+        return request
+                .body(user)
+                .post(Constants.USER_CREATE_REQUEST);
+
+    }
+
+    @Step("Delete a user with DELETE request to /api/auth/user")
+    public static Response delete(String bearerToken) {
+        return request
+                .headers(
+                        "Authorization",
+                        "Bearer " + bearerToken)
+                .delete(Constants.USER_DELETE_REQUEST);
+
+    }
+
+    @Step("Login a user with POST request to /api/auth/login")
+    public static Response login(UserForLogin user, String bearerToken) {
+        return request
+                .body(user)
+                .post(Constants.USER_LOGIN_REQUEST);
+    }
 }
