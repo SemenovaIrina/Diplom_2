d99ebaa21aa0881a4eef054a0f268fdfc4db7c8d
Irina Semenova <semenova.iv.ssau@ya.ru>
2024-11-21 20:54:30 +0400

    add builder

--- src/main/java/edu/practicum/models/UserForLogin.java
+++ src/main/java/edu/practicum/models/UserForLogin.java
@@ -4,9 +4,28 @@ public class UserForLogin {
     private String email;
     private String password;
 
-    public UserForLogin(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public static class Builder {
+        private String email = null;
+        private String password = null;
+
+        public UserForLogin.Builder email(String val) {
+            this.email = val;
+            return this;
+        }
+
+        public UserForLogin.Builder password(String val) {
+            this.password = val;
+            return this;
+        }
+
+        public UserForLogin build() {
+            return new UserForLogin(this);
+        }
+    }
+
+    public UserForLogin(UserForLogin.Builder builder) {
+        email = builder.email;
+        password = builder.password;
     }
 
     public String getEmail() {
